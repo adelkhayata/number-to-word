29eb3d0b9048135011da26dbfa5cb55cb42cdfe7
Adel Khayata <adel@khayata.com>
2024-01-27 07:31:56 +0400

    Pumped verion to 1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='number-to-word',
-    version='1.0',
+    version='1.1',
     packages=find_packages(),
     description='number-to-word is a Python library for converting numbers into their written English or Arabic word equivalents.',
     long_description=long_description,
